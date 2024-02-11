622c9301bec6d62b7386e0a6b3d6adfcd4061747
Mehdi <elmahdi20erraji@gmail.com>
2024-02-11 16:33:46 +0100

    Added factories, seeders, and relationships for roles, permissions, and users

---
+++ database/factories/PermissionFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Permission;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PermissionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            
+            'name' => $this->faker->randomElement(['can delete', 'can update', 'can create', 'can view']),
+        ];
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,15 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+           
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'description' => $this->faker->sentence,
+            'profession' => $this->faker->jobTitle,
+            'phone' => $this->faker->phoneNumber,
+            'status' => $this->faker->randomElement([0, 1, 2]),
+            
         ];
     }
 
