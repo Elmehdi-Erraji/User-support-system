ac961d078466bd8f9f37d6c68239c568f9f4475d
Mehdi <elmahdi20erraji@gmail.com>
2024-04-02 23:52:28 +0000

    quick change

--- resources/views/dashboard/admin/dashboard.blade.php
+++ resources/views/dashboard/admin/dashboard.blade.php
@@ -150,13 +150,6 @@
         </div>
     </div> --}}
 
-    <div class="col-md-3 col-sm-6" style="display: none">
-        <div class="text-center p-1 pb-3 p-sm-3">
-            <p>Info Example</p>
-            <button type="button" class="btn btn-info btn-sm" id="toastr-one">Click
-                me</button>
-        </div>
-    </div>
     {{-- <div class="row">
         <div class="col-12">
             <div class="card">
---
+++ resources/views/partials/toasters.blade.php
@@ -0,0 +1,8 @@
+
+<div class="col-md-3 col-sm-6" style="display: none">
+    <div class="text-center p-1 pb-3 p-sm-3">
+        <p>Info Example</p>
+        <button type="button" class="btn btn-info btn-sm" id="toastr-one">Click
+            me</button>
+    </div>
+</div>
